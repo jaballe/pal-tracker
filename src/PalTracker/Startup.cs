338d94241cce48832fbe2d515610f4820d716d25
Beta Team <user01@win01.(none)>
2020-08-26 05:02:30 +0000

    Adding Steeltoe Management library

--- src/PalTracker/Startup.cs
+++ src/PalTracker/Startup.cs
@@ -11,6 +11,10 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Steeltoe.CloudFoundry.Connector.MySql.EFCore;
+using Steeltoe.Management.Endpoint.CloudFoundry;
+using Steeltoe.Management.CloudFoundry;
+using Steeltoe.Common.HealthChecks;
+using Steeltoe.Management.Endpoint.Info;
 
 namespace PalTracker
 {
@@ -19,6 +23,7 @@ namespace PalTracker
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+
         }
 
         public IConfiguration Configuration { get; }
@@ -26,6 +31,8 @@ namespace PalTracker
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddCloudFoundryActuators(Configuration, MediaTypeVersion.V2, ActuatorContext.ActuatorAndCloudFoundry);
+            services.AddCloudFoundryActuators(Configuration);
             services.AddControllers();
 
             var message = Configuration.GetValue<string>("WELCOME_MESSAGE");
@@ -54,6 +61,9 @@ namespace PalTracker
             services.AddDbContext<TimeEntryContext>(options => options.UseMySql(Configuration));
             services.AddScoped<ITimeEntryRepository, MySqlTimeEntryRepository>();
 
+            services.AddScoped<IHealthContributor, TimeEntryHealthContributor>();
+            services.AddSingleton<IOperationCounter<TimeEntry>, OperationCounter<TimeEntry>>();
+            services.AddSingleton<IInfoContributor, TimeEntryInfoContributor>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -63,6 +73,8 @@ namespace PalTracker
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseCloudFoundryActuators();
 
             app.UseHttpsRedirection();
 
